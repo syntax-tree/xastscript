21a360aba8fc4dc2eefaa5aeffe437cc23adcb6d
Stefan Probst <stefanprobst@kolabnow.com>
2020-10-03 13:05:42 +0200

    chore: add types

---
+++ types/test.ts
@@ -0,0 +1,21 @@
+import x = require('xastscript')
+
+x('urlset') // $ExpectType Element
+x('urlset', 'string') // $ExpectType Element
+x('urlset', ['string', 'string']) // $ExpectType Element
+x('urlset', x('loc')) // $ExpectType Element
+x('urlset', [x('loc'), x('loc')]) // $ExpectType Element
+x('urlset', []) // $ExpectType Element
+
+const xmlns = 'http://www.sitemaps.org/schemas/sitemap/0.9'
+
+x('urlset', {xmlns}) // $ExpectType Element
+x('urlset', {xmlns}, 'string') // $ExpectType Element
+x('urlset', {xmlns}, ['string', 'string']) // $ExpectType Element
+x('urlset', {xmlns}, x('loc')) // $ExpectType Element
+x('urlset', {xmlns}, [x('loc'), x('loc')]) // $ExpectType Element
+x('urlset', {xmlns}, []) // $ExpectType Element
+
+x() // $ExpectError
+x(false) // $ExpectError
+x('urlset', x('loc'), {xmlns}) // $ExpectError
