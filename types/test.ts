368430c752f1fed2a514282385a6fa3d48839910
Remco Haszing <remcohaszing@gmail.com>
2020-10-30 12:37:37 +0100

    Fix types for children

--- types/test.ts
+++ types/test.ts
@@ -3,7 +3,9 @@ import x = require('xastscript')
 x('urlset') // $ExpectType Element
 x('urlset', 'string') // $ExpectType Element
 x('urlset', ['string', 'string']) // $ExpectType Element
+x('urlset', x('loc'), 'string') // $ExpectType Element
 x('urlset', x('loc')) // $ExpectType Element
+x('urlset', x('loc'), x('loc')) // $ExpectType Element
 x('urlset', [x('loc'), x('loc')]) // $ExpectType Element
 x('urlset', []) // $ExpectType Element
 
@@ -12,7 +14,9 @@ const xmlns = 'http://www.sitemaps.org/schemas/sitemap/0.9'
 x('urlset', {xmlns}) // $ExpectType Element
 x('urlset', {xmlns}, 'string') // $ExpectType Element
 x('urlset', {xmlns}, ['string', 'string']) // $ExpectType Element
+x('urlset', {xmlns}, x('loc'), 'string') // $ExpectType Element
 x('urlset', {xmlns}, x('loc')) // $ExpectType Element
+x('urlset', {xmlns}, x('loc'), x('loc')) // $ExpectType Element
 x('urlset', {xmlns}, [x('loc'), x('loc')]) // $ExpectType Element
 x('urlset', {xmlns}, []) // $ExpectType Element
 
