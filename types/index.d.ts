73d2f25510278a9ec681a169c6cdc4628f8d74fa
Remco Haszing <remcohaszing@gmail.com>
2020-10-31 12:57:16 +0100

    Add JSX support in type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,8 @@
 // TypeScript Version: 3.7
 
-import {Attributes, Element, Node} from 'xast'
+import * as xast from 'xast'
 
-type Children = string | Node | Children[]
+type Children = string | xast.Node | Children[]
 
 /**
  * Create XML trees in xast.
@@ -10,7 +10,7 @@ type Children = string | Node | Children[]
  * @param name Qualified name. Case sensitive and can contain a namespace prefix (such as rdf:RDF).
  * @param children (Lists of) child nodes. When strings are encountered, they are mapped to Text nodes.
  */
-declare function xastscript(name: string, ...children: Children[]): Element
+declare function xastscript(name: string, ...children: Children[]): xast.Element
 
 /**
  * Create XML trees in xast.
@@ -21,8 +21,49 @@ declare function xastscript(name: string, ...children: Children[]): Element
  */
 declare function xastscript(
   name: string,
-  attributes?: Attributes,
+  attributes?: xast.Attributes,
   ...children: Children[]
-): Element
+): xast.Element
+
+/**
+ * This namespace allows to use `xastscript` as a JSX implementation.
+ *
+ * This namespace is only used to support the use as JSX. It’s **not** intended for direct usage.
+ */
+declare namespace xastscript.JSX {
+  /**
+   * This defines the return value of JSX syntax.
+   */
+  type Element = xast.Element
+
+  /**
+   * This disallows the use of
+   */
+  type IntrinsicAttributes = never
+
+  /**
+   * This defines the prop types for known elements.
+   *
+   * For `xastscript` this defines any string may be used in combination with `xast` `Attributes`.
+   */
+  interface IntrinsicElements {
+    [key: string]: xast.Attributes & {
+      /**
+       * The prop that matches `ElementChildrenAttribute` key defines the type of JSX children, defines the children type.
+       */
+      ''?: Children
+    }
+  }
+
+  /**
+   * The key of this interface  defines as what prop children are passed.
+   */
+  interface ElementChildrenAttribute {
+    /**
+     * Only the key matters, not the value.
+     */
+    '': never
+  }
+}
 
 export = xastscript
--- types/test.tsx
+++ types/test.tsx
@@ -1,3 +1,6 @@
+/** @jsx x */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {Attributes, Element} from 'xast'
 import x = require('xastscript')
 
 x('urlset') // $ExpectType Element
@@ -20,6 +23,41 @@ x('urlset', {xmlns}, x('loc'), x('loc')) // $ExpectType Element
 x('urlset', {xmlns}, [x('loc'), x('loc')]) // $ExpectType Element
 x('urlset', {xmlns}, []) // $ExpectType Element
 
+// $ExpectType Element
+const jsx0 = <urlset />
+// $ExpectType Element
+const jsx1 = <urlset xmlns={xmlns} />
+// $ExpectType Element
+const jsx2 = <urlset>string</urlset>
+// $ExpectType Element
+const jsx3 = <urlset>{['string', 'string']}</urlset>
+// $ExpectType Element
+const jsx4 = (
+  <urlset>
+    <loc />
+    string
+  </urlset>
+)
+// $ExpectType Element
+const jsx5 = (
+  <urlset>
+    <loc />
+  </urlset>
+)
+// $ExpectType Element
+const jsx6 = (
+  <urlset>
+    <loc />
+    <loc />
+  </urlset>
+)
+// $ExpectType Element
+const jsx7 = <urlset>{[<loc />, <loc />]}</urlset>
+// $ExpectType Element
+const jsx8 = <urlset>{[]}</urlset>
+
+declare function Bar(props?: Attributes): Element
+const bar = <Bar /> // $ExpectError
 x() // $ExpectError
 x(false) // $ExpectError
 x('urlset', x('loc'), {xmlns}) // $ExpectError
