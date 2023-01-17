d0c8e885629af52a40a98eb9f0c5ff625145998b
Titus Wormer <tituswormer@gmail.com>
2023-01-17 15:01:32 +0100

    Remove superfluous dev-dependencies

--- script/generate-jsx.js
+++ script/generate-jsx.js
@@ -1,87 +1,50 @@
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
-import babel from '@babel/core'
-import {Parser} from 'acorn'
 import acornJsx from 'acorn-jsx'
+import {fromJs} from 'esast-util-from-js'
 import {toJs} from 'estree-util-to-js'
 import {buildJsx} from 'estree-util-build-jsx'
 
-const doc = String(fs.readFileSync(path.join('test', 'jsx.jsx')))
+const doc = String(await fs.readFile(path.join('test', 'jsx.jsx')))
 
-fs.writeFileSync(
+await fs.writeFile(
   path.join('test', 'jsx-build-jsx-classic.js'),
   toJs(
-    // @ts-expect-error it’s a program.
     buildJsx(
-      // @ts-expect-error Acorn nodes are assignable to ESTree nodes.
-      Parser.extend(acornJsx()).parse(
-        doc.replace(/'name'/, "'jsx (estree-util-build-jsx, classic)'"),
-        // Note: different npms resolve this differently, so it may break or work, hence the ignore.
-        // @ts-ignore Hush, `2021` is fine.
-        {sourceType: 'module', ecmaVersion: 2021}
-      ),
+      fromJs(doc.replace(/'name'/, "'jsx (estree-util-build-jsx, classic)'"), {
+        plugins: [acornJsx()],
+        module: true
+      }),
       {pragma: 'x', pragmaFrag: 'null'}
     )
-    // @ts-expect-error Some bug in `to-js`
   ).value
 )
 
-fs.writeFileSync(
+await fs.writeFile(
   path.join('test', 'jsx-build-jsx-automatic.js'),
   toJs(
-    // @ts-expect-error it’s a program.
     buildJsx(
-      // @ts-expect-error Acorn nodes are assignable to ESTree nodes.
-      Parser.extend(acornJsx()).parse(
+      fromJs(
         doc.replace(/'name'/, "'jsx (estree-util-build-jsx, automatic)'"),
-        // Note: different npms resolve this differently, so it may break or work, hence the ignore.
-        // @ts-ignore Hush, `2021` is fine.
-        {sourceType: 'module', ecmaVersion: 2021}
+        {plugins: [acornJsx()], module: true}
       ),
       {runtime: 'automatic', importSource: 'xastscript'}
     )
-    // @ts-expect-error Some bug in `to-js`
   ).value
 )
 
-fs.writeFileSync(
+await fs.writeFile(
   path.join('test', 'jsx-build-jsx-automatic-development.js'),
   toJs(
-    // @ts-expect-error it’s a program.
     buildJsx(
-      // @ts-expect-error Acorn nodes are assignable to ESTree nodes.
-      Parser.extend(acornJsx()).parse(
-        doc.replace(/'name'/, "'jsx (estree-util-build-jsx, automatic)'"),
-        {sourceType: 'module', ecmaVersion: 2021}
+      fromJs(
+        doc.replace(
+          /'name'/,
+          "'jsx (estree-util-build-jsx, automatic, development)'"
+        ),
+        {plugins: [acornJsx()], module: true}
       ),
       {runtime: 'automatic', importSource: 'xastscript', development: true}
     )
-    // @ts-expect-error Some bug in `to-js`
   ).value
-)
-
-fs.writeFileSync(
-  path.join('test', 'jsx-babel-automatic.js'),
-  // @ts-expect-error Result always given.
-  babel.transformSync(doc.replace(/'name'/, "'jsx (babel, automatic)'"), {
-    plugins: [
-      [
-        '@babel/plugin-transform-react-jsx',
-        {runtime: 'automatic', importSource: 'xastscript'}
-      ]
-    ]
-  }).code
-)
-
-fs.writeFileSync(
-  path.join('test', 'jsx-babel-automatic-development.js'),
-  // @ts-expect-error Result always given.
-  babel.transformSync(doc.replace(/'name'/, "'jsx (babel, automatic)'"), {
-    plugins: [
-      [
-        '@babel/plugin-transform-react-jsx',
-        {runtime: 'automatic', importSource: 'xastscript', development: true}
-      ]
-    ]
-  }).code
 )
